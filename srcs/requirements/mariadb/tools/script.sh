cd5db351fca3130f2f5aca0d765d19d941eee9f6
rida2025 <ridaeljirari@gmail.com>
2024-08-03 14:54:14 +0100

    another try to my trying list

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-systemctl start mariadb
+service mysql start
 
 echo "CREATE DATABASE IF NOT EXISTS $db1_name ;" > db1.sql
 echo "CREATE USER IF NOT EXISTS '$db1_user'@'%' IDENTIFIED BY '$db1_pwd' ;" >> db1.sql
