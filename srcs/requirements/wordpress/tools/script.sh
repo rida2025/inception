c056db059ad9f179b1836530f5c462f4d97f6136
rida2025 <ridaeljirari@gmail.com>
2024-08-03 18:11:27 +0100

    did i finish mandatory ???

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -13,11 +13,11 @@ wp core config --dbhost=mariadb --dbname=$db_name --dbuser=$db_user --dbpass=$db
 wp core install --url=$DOMAIN_NAME --title="$WP_TITLE" --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --allow-root
 wp user create $WP_USR $WP_EMAIL --user_pass=$WP_PWD --role=$WP_ROLE --allow-root
 
-wp plugin install redis-cache --activate --allow-root
-sleep 10
-wp config set WP_REDIS_HOST redis --allow-root
-wp config set WP_REDIS_PORT 6379 --raw --allow-root
-wp redis enable --allow-root
+#wp plugin install redis-cache --activate --allow-root
+#sleep 10
+#wp config set WP_REDIS_HOST redis --allow-root
+#wp config set WP_REDIS_PORT 6379 --raw --allow-root
+#wp redis enable --allow-root
 
 sed -i 's|^listen = /run/php/php7.4-fpm.sock|listen = 9000|' /etc/php/7.4/fpm/pool.d/www.conf
 php-fpm7.4 -F
