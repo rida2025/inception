0eb450ea244504c83d0c5a453a0e54817629e858
rida2025 <ridaeljirari@gmail.com>
2024-08-06 20:01:30 +0100

    changes hoping to fix

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -2,10 +2,10 @@
 
 service vsftpd start
 
-useradd -m mel-jira
+useradd -m $ftp_user
 
 echo "$ftp_user:$ftp_pwd" | /usr/sbin/chpasswd
 
-chown -R mel-jira /home/mel-jira
+chown -R $ftp_user /home/$ftp_user
 
 vsftpd /etc/vsftpd.conf
